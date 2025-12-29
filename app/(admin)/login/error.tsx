fb0777c78af117f15dcca99c19d65d72487241e5
Vishal <dubeyvishal714@gmail.com>
2025-12-29 23:42:24 +0530

    learn error handling

---
+++ app/(admin)/login/error.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+const ErrorPage = () => {
+    return (
+        <div>Error aa gya bhai</div>
+    );
+};
+
+export default ErrorPage;
---
+++ app/(admin)/login/page.tsx
@@ -0,0 +1,10 @@
+import react from "react" ; 
+
+const Login = ()=>{
+    throw Error("Invalid credientials ");
+    return (
+        <div>Welcome to login page </div>
+    )
+}
+
+export default Login ;
